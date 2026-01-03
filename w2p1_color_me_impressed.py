721969aabaae3a4422cb84b0d8a4f5e3d2427819
olivcho <olivercho007@gmail.com>
2026-01-03 22:25:48 +0900

    finish color_me_impressed.py

--- w2p1_color_me_impressed.py
+++ w2p1_color_me_impressed.py
@@ -16,10 +16,25 @@ print(IMAGE_PATH)
 
 img = cv2.imread(IMAGE_PATH)
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Original", img)
 
-# Display the image
-cv2.imshow("Color Me Impressed", img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+LOWER_BOUND = np.array([0, 40, 80])
+UPPER_BOUND = np.array([25, 170, 255])
+
+mask = cv2.inRange(img, LOWER_BOUND, UPPER_BOUND)
+cv2.imshow("Mask", mask)
+
+# Print center of mass of the result
+M = cv2.moments(mask)
+center_of_mass = (int(M['m10']/M['m00']), int(M['m01']/M['m00']))
+print(f"Center of object: {center_of_mass}")
+
+result = cv2.bitwise_and(img, img, mask=mask)
+result = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
+cv2.circle(result, center_of_mass, 10, (0, 0, 255), -1)
+cv2.imshow("Result", result)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
